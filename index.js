6614e0bfb816996848c3a8a930b84f3a91e7270f
juanpablosys <juanpablosys@gmail.com>
2022-06-22 01:47:50 -0300

    cjs-desafio-complementario-entregable-2

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const iva = a => a * 0.21;
 const certificadoImpreso = a => a * 0.1;
 const suma = (a, b, c) => a + b + c;
 
+class Cursada{
+    constructor(id,nombre,costo) {
+        this.id = parseInt(id);
+        this.nombre  = nombre.toUpperCase();
+        this.costo  = parseFloat(costo);
+    }
+}
+
+const cursadas = [];
+cursadas.push(new Cursada("1","EntrenamientoFuncional","2000"));
+cursadas.push(new Cursada("2","RitmosLatinos", "1500"));
+const curso1 = cursadas.filter(item => item.id === 1);
+const curso2 = cursadas.filter(item => item.id === 2);
+
 let nombreCliente;
 let seleccionLista;
 let costoCursoEntrenamientoFuncional = 2000;
@@ -15,7 +29,7 @@ lista += "Elija una opccion\n";
 
 let cursos =
     "Lista de Cursos\n";
-cursos += "1. Entrenamiento funcional\n";
+cursos += "1. Entrenamiento Funcional\n";
 cursos += "2. Ritmos Latinos\n";
 cursos += "Elija una opccion\n";
 
